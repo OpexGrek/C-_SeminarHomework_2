43e3d4165f5cc572bfe9b66535058133845c0bcd
Oleg <starwarser2@gmail.com>
2022-12-03 10:56:43 +0700

    Task16 является ли одно число квадратом другого

---
+++ Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Input number A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input number B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (B == A * A || A == B * B)
+    Console.Write("Yes");
+else
+    Console.Write("No");
