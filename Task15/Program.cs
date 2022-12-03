335b50e356ea562bac43b3e790f78a221de244d6
Oleg <starwarser2@gmail.com>
2022-12-03 10:40:20 +0700

    Task15 определяет выходной день

---
+++ Task15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Input day number (1-7): ");
+int DayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if (DayOfWeek == 6 || DayOfWeek == 7)
+    Console.WriteLine("Weekend");
+else
+    Console.WriteLine("Working day");
