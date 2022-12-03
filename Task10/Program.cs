ece89ffa1e1d8336c87cc452c638cdfca851f0eb
Oleg <starwarser2@gmail.com>
2022-12-03 09:53:22 +0700

    Task10 show the second number

---
+++ Task10/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Input 3 number ");
+int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(ThreeDigitNumber);
+Console.Write("The second number " + stringNumber[1]);
