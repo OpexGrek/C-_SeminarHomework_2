d3d15f591c3188f7aa5e6116179eee2c462e0967
Oleg <starwarser2@gmail.com>
2022-12-03 10:11:17 +0700

    Task13 показывает отсутствие третьей цифры

---
+++ Task13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input number ");
+int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(ThreeDigitNumber);
+
+int lenghtNumber = stringNumber.Length;
+Console.WriteLine(lenghtNumber);
+
+if (lenghtNumber == 2) 
+    Console.WriteLine("There is no 3-d number");
+else 
+    Console.Write("The second number is " + stringNumber[1]);
