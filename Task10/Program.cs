fb91733052911688b6e348f4e5ad23145573eebd
Oleg <starwarser2@gmail.com>
2022-12-04 13:07:28 +0700

    Добавление описания условий к задачe

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Write("Input 3 number ");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Input 3 number ");
 int ThreeDigitNumber = Convert.ToInt32(Console.ReadLine());
 string stringNumber = Convert.ToString(ThreeDigitNumber);
 if(stringNumber.Length !=3)
