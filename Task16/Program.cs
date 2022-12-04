7b73377841708f528c30d48719f51d81daee6c5d
Oleg <starwarser2@gmail.com>
2022-12-04 13:07:53 +0700

    Добавление описания условий к задачe

--- Task16/Program.cs
+++ Task16/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.Clear();
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+Console.Clear();
 Console.Write("Input number A: ");
 int A = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input number B: ");
